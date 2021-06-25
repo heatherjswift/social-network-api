815f3564a00f018f69f57f176d6eeef7d7a375ea
Heather Swift <78330855+heatherjswift@users.noreply.github.com>
2021-06-25 14:14:09 -0400

    create user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: "Username is required!",
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: "E-mail is required!",
+      validate: [/.+@.+\..+/, 'Please enter a valid e-mail address.']
+    },
+    // array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    // array of _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+// create User model using UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
