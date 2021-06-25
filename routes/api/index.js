e169b7024363477c540db21fbb53922842aa98e9
Heather Swift <78330855+heatherjswift@users.noreply.github.com>
2021-06-25 18:38:25 -0400

    test all user routes successfully

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
 // add prefix of `/users` to routes created in `user-routes.js`
-router.use('./users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
